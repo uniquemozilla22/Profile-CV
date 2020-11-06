d519d5f82b73d86aaea79d05a956df6ac63afa1e
uniquemozilla22 <49606627+uniquemozilla22@users.noreply.github.com>
2020-11-06 21:38:14 +0545

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Body from './Components/Body/Body';
+
+function App() {
+  return (
+    <div className="App">
+    <Body></Body>
+     </div>
+  );
+}
+
+export default App;
